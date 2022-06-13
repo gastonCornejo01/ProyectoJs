80ddaf557f221d91e8a6e140c08a509e613881f1
rdanielpolich <rdanielpolich@gmail.com>
2022-06-13 18:47:37 -0300

    rama main actualizada

--- js/home.js
+++ js/home.js
@@ -1,49 +1,105 @@
-let usuario = JSON.parse(localStorage.getItem("user")) || null;
-let contenedorLista = document.getElementById("menu_lista");
+let registrosIngreso =
+  JSON.parse(localStorage.getItem("registrosIngreso")) || [];
+let registrosEgreso = JSON.parse(localStorage.getItem("registrosEgreso")) || [];
+let tableBody = document.querySelector("#table_body");
+let tableBody1 = document.querySelector("#table_body1");
+let containerBalance = document.getElementById("container_balance");
+let containerIngreso = document.getElementById("container_ingreso");
+let containerEgreso = document.getElementById("container_egreso");
 
-//contenedor de las tarjetas
-let contenedorTarjetas = document.getElementById("tarjetas_container");
+const sumaIngreso = registrosIngreso
+  .map((Registro) => Number(Registro.monto))
+  .reduce((prev, curr) => prev + curr, 0);
+console.log(sumaIngreso);
 
-//Traer los datos de los cursos desde localstorage
-let cursos = JSON.parse(localStorage.getItem("cursos")) || [];
+const sumaEgreso = registrosEgreso
+  .map((Registro) => Number(Registro.monto))
+  .reduce((prev, curr) => prev + curr, 0);
+console.log(sumaEgreso);
 
+const balanceTotal = sumaIngreso - sumaEgreso;
+console.log(balanceTotal);
 
+const cargarIngreso = () => {
+  containerIngreso.innerHTML = "";
+  let TotalIngresos = sumaIngreso;
+  let div = document.createElement("div");
+  div.classList = "card-body";
+  let contenidoCard = `<div class="card" style="width: 18rem;">
+  <img src="..." class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Total de Ingresos</h5>
+    <h1 class="card-text">${TotalIngresos}</h1>
+   </div>
+</div>`;
+  div.innerHTML = contenidoCard;
+  containerIngreso.appendChild(div);
+};
 
-//Cargar los cursos en tarjetas
-function cargarTarjetas() {
-  //limpiar el contenedor
-  contenedorTarjetas.innerHTML = "";
-
-  if (cursos.length > 0) {
-    //recorrer el arreglo de cursos y crear cada tarjeta
-    cursos.map(function (curso) {
-      let div = document.createElement("div");
-      div.classList = "col-12 col-md-4 mb-3";
-
-      let tarjeta = `<div class="card h-100">
-      <img src="${curso.imagen}" class="card-img-top img_tarjeta" alt="${curso.titulo}">
-      <div class="card-body">
-        <h5 class="card-title">${curso.titulo}</h5>
-        <p class="card-text">${curso.descripcion}</p>
-       
-      </div>
-        <div class="card-footer">
-        <a href="#" class="btn btn-primary float-end">Comprar</a>
-        </div>
-      </div>`;
-
-      div.innerHTML = tarjeta;
-      contenedorTarjetas.appendChild(div);
-    });
-  } else {
-    let div = document.createElement("div");
-    let alerta = ` <div class="alert alert-warning" role="alert">
-    No hay cursos habilitados
+const cargarEgreso = () => {
+  containerEgreso.innerHTML = "";
+  let TotalEgresos = sumaEgreso;
+  let div = document.createElement("div");
+  div.classList = "card-body";
+  let contenidoCard = `<div class="card" style="width: 18rem;">
+    <img src="..." class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">Total de Egresos</h5>
+      <h1 class="card-text">${TotalEgresos}</h1>
+     </div>
   </div>`;
-    div.innerHTML = alerta;
-    contenedorTarjetas.appendChild(div);
-  }
-}
-//-----------------------------------------------
+  div.innerHTML = contenidoCard;
+  containerEgreso.appendChild(div);
+};
 
-cargarTarjetas();
+const cargarBalance = () => {
+  containerBalance.innerHTML = "";
+  let TotalBalance = sumaIngreso - sumaEgreso;
+  let div = document.createElement("div");
+  div.classList = "card-body";
+  let contenidoCard = `<div class="card" style="width: 18rem;">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Balance Total</h5>
+        <h1 class="card-text">${TotalBalance}</h1>
+       </div>
+    </div>`;
+  div.innerHTML = contenidoCard;
+  containerBalance.appendChild(div);
+};
+
+const cargarTabla = () => {
+  tableBody.innerHTML = "";
+  registrosIngreso.map(function (registro) {
+    let tr = document.createElement("tr");
+    tr.classList = "table-success";
+    let celda = `<th scope="row">${registro.fecha}</th>
+              <td>${registro.nombre}</td>
+              <td class="d-flex justify-content-between align-items-center">${registro.monto}</td>`;
+
+    tr.innerHTML = celda;
+    tableBody.appendChild(tr);
+  });
+};
+
+const cargarTabla1 = () => {
+  tableBody1.innerHTML = "";
+  registrosEgreso.map(function (registro) {
+    let tr1 = document.createElement("tr");
+    tr1.classList = "table-danger";
+    let celda = `<th scope="row">${registro.fecha}</th>
+          <td>${registro.nombre}</td>
+          <td class="d-flex justify-content-between align-items-center">${registro.monto}
+          </td>`;
+
+    tr1.innerHTML = celda;
+    tableBody1.appendChild(tr1);
+    cargarTabla();
+  });
+};
+
+cargarTabla();
+cargarTabla1();
+cargarBalance();
+cargarIngreso();
+cargarEgreso();

d7f8e042c604af81cb37f3d04894636336e40287
rdanielpolich <rdanielpolich@gmail.com>
2022-06-13 17:32:02 -0300

    creada la logica de la pagina de administracion

--- js/administracion.js
+++ js/administracion.js
@@ -1,64 +1,48 @@
-class Ingresos {
-  constructor(id, fecha, nombre, monto) {
+class Registro {
+  constructor(id, fecha, nombre, monto, tipo) {
     this.id = id;
     this.fecha = fecha;
     this.nombre = nombre;
     this.monto = monto;
+    this.tipo = tipo;
   }
 }
 
-class Egresos {
-  constructor(id1, fecha1, nombre1, monto1) {
-    this.id = id1;
-    this.fecha = fecha1;
-    this.nombre = nombre1;
-    this.monto1 = monto1;
-  }
-}
-
-let ingresos = JSON.parse(localStorage.getItem("ingresos")) || [];
+let registrosIngreso =
+  JSON.parse(localStorage.getItem("registrosIngreso")) || [];
+let registrosEgreso = JSON.parse(localStorage.getItem("registrosEgreso")) || [];
 let tableBody = document.querySelector("#table_body");
-let egresos = JSON.parse(localStorage.getItem("egresos")) || [];
 let tableBody1 = document.querySelector("#table_body1");
 
 let myModal = new bootstrap.Modal(document.getElementById("myModal"));
+let myModal1 = new bootstrap.Modal(document.getElementById("myModal"));
 
-function agregarIngresos(e) {
+function agregarRegistros(e) {
   e.preventDefault();
 
   let id = new Date().getTime();
   let fecha = document.getElementById("text_fecha").value;
   let nombre = document.getElementById("text_nombre").value;
   let monto = document.getElementById("text_monto").value;
+  let tipo = document.getElementById("tipo").value;
 
   if (monto < 0) {
     return alert("El monto ingresado es inválido");
   }
 
-  ingresos.push(new Ingresos(id, fecha, nombre, monto));
-  localStorage.setItem("ingresos", JSON.stringify(ingresos));
-  document.getElementById("formulario_ingreso").reset();
-  document.getElementById("text_nombre").focus();
-  cargarTabla();
-}
-
-function agregarEgresos(e) {
-  e.preventDefault();
-
-  let id1 = new Date().getTime();
-  let fecha1 = document.getElementById("text_fecha1").value;
-  let nombre1 = document.getElementById("text_nombre1").value;
-  let monto1 = document.getElementById("text_monto1").value;
-
-  if (monto1 < 0) {
-    return alert("El monto ingresado es inválido");
+  if (tipo === "Ingreso") {
+    registrosIngreso.push(new Registro(id, fecha, nombre, monto, tipo));
+    localStorage.setItem("registrosIngreso", JSON.stringify(registrosIngreso));
+    cargarTabla();
+    document.getElementById("formulario_ingreso").reset();
+    document.getElementById("text_nombre").focus();
+  } else {
+    registrosEgreso.push(new Registro(id, fecha, nombre, monto, tipo));
+    localStorage.setItem("registrosEgreso", JSON.stringify(registrosEgreso));
+    cargarTabla1();
+    document.getElementById("formulario_ingreso").reset();
+    document.getElementById("text_nombre").focus();
   }
-
-  egresos.push(new Egresos(id1, fecha1, nombre1, monto1));
-  localStorage.setItem("egresos", JSON.stringify(egresos));
-  document.getElementById("formulario_egreso").reset();
-  document.getElementById("text_nombre1").focus();
-  cargarTabla1();
 }
 
 const editModal = function (registroId) {
@@ -66,70 +50,202 @@ const editModal = function (registroId) {
   crearCuerpoModal(registroId);
 };
 
-const crearCuerpoModal = (index) => {
-  document.querySelector(".modal-body").innerHTML = "";
+const editModal1 = function (registroId) {
+  myModal1.show();
+  crearCuerpoModal1(registroId);
+};
 
-  let bodyModal = document.querySelector(".modal-body");
-  let contenidoBody = `<form id=form-update onSubmit="actualizarIngreso(event,${index})">
-  <label>Fecha<label>
-  <input id="fecha-update" class="form-control" type="date" value=${ingresos[index].fecha}" required/>
-  <label>Descripción<label>
-  <input id="nombre-update" class="form-control" type="text" value=${ingresos[index].nombre}" required/>
-  <label>Monto<label>
-  <input id="monto-update" class="form-control" type="number" value=${ingresos[index].monto}" required/>
-  <button class="btn btn-primary mt-3 float-end">Guardar</button>
-  </form>`;
+const crearCuerpoModal = (index) => {
+  document.querySelector(".modal-body1").innerHTML = "";
+
+  let bodyModal = document.querySelector(".modal-body1");
+  let contenidoBody = `<form id="form-update" onSubmit="actualizarIngreso(event,${index})">
+  <label class="fw-bold">Descripcion</label>
+  <input
+    type="text"
+    value="${registrosIngreso[index].nombre}"
+    class="form-control"
+    id="nombre-update"
+    required
+  />
+  <label class="fw-bold">Monto</label>
+  <input
+    type="number"
+    value="${registrosIngreso[index].monto}"
+    class="form-control"
+    id="monto-update"
+    required
+  />
+  <label class="fw-bold">Fecha</label>
+  <input
+    type="date"
+    class="form-control"
+    value="${registrosIngreso[index].fecha}"
+    id="fecha-update"
+    required
+  />
+   
+  <button
+    type="button"
+    class="btn btn-secondary"
+    data-bs-dismiss="modal"
+  >
+    Cancelar
+  </button>
+  <button type="submit" class="btn btn-success float-end">
+    Actualizar
+  </button>
+</form>`;
 
   bodyModal.innerHTML = contenidoBody;
 };
 
+const crearCuerpoModal1 = (index) => {
+  document.querySelector(".modal-body1").innerHTML = "";
+
+  let bodyModal1 = document.querySelector(".modal-body1");
+  let contenidoBody1 = `<form id="form-update" onSubmit="actualizarEgreso(event,${index})">
+  <label class="fw-bold">Descripcion</label>
+  <input
+    type="text"
+    value="${registrosEgreso[index].nombre}"
+    class="form-control"
+    id="nombre-update"
+    required
+  />
+  <label class="fw-bold">Monto</label>
+  <input
+    type="number"
+    value="${registrosEgreso[index].monto}"
+    class="form-control"
+    id="monto-update"
+    required
+  />
+  <label class="fw-bold">Fecha</label>
+  <input
+    type="date"
+    class="form-control"
+    value="${registrosEgreso[index].fecha}"
+    id="fecha-update"
+    required
+  />
+   
+  <button
+    type="button"
+    class="btn btn-secondary"
+    data-bs-dismiss="modal"
+  >
+    Cancelar
+  </button>
+  <button type="submit" class="btn btn-success float-end">
+    Actualizar
+  </button>
+</form>`;
+
+  bodyModal1.innerHTML = contenidoBody1;
+};
+
 const actualizarIngreso = function (e, index) {
   e.preventDefault();
+  // console.log(index);
 
   let fecha = document.getElementById("fecha-update").value;
+  console.log(fecha);
   let nombre = document.getElementById("nombre-update").value;
+  console.log(nombre);
   let monto = document.getElementById("monto-update").value;
+  console.log(monto);
 
   const newData = {
     fecha,
     nombre,
     monto,
   };
-  ingresos.splice(index, 1, newData);
-  localStorage.setItem("ingresos", JSON.stringify(ingresos));
+  registrosIngreso.splice(index, 1, newData);
+  localStorage.setItem("registrosIngreso", JSON.stringify(registrosIngreso));
   myModal.hide();
   cargarTabla();
 };
 
+const actualizarEgreso = function (e, index) {
+  e.preventDefault();
+  // console.log(index);
+
+  let fecha = document.getElementById("fecha-update").value;
+  console.log(fecha);
+  let nombre = document.getElementById("nombre-update").value;
+  console.log(nombre);
+  let monto = document.getElementById("monto-update").value;
+  console.log(monto);
+
+  const newData = {
+    fecha,
+    nombre,
+    monto,
+  };
+  registrosEgreso.splice(index, 1, newData);
+  localStorage.setItem("registrosEgreso", JSON.stringify(registrosEgreso));
+  myModal.hide();
+  cargarTabla1();
+};
+
+const borrarIngreso = (registroId) => {
+  let validar = confirm(
+    `Está seguro que desea eliminar el registro ${registrosIngreso[registroId].nombre}?`
+  );
+
+  if (validar) {
+    registrosIngreso.splice(registroId, 1);
+    localStorage.setItem("registrosIngreso", JSON.stringify(registrosIngreso));
+    alert("Registro eliminado");
+    cargarTabla();
+  }
+};
+
+const borrarEgreso = (registroId) => {
+  let validar = confirm(
+    `Está seguro que desea eliminar el registro ${registrosEgreso[registroId].nombre}?`
+  );
+
+  if (validar) {
+    registrosEgreso.splice(registroId, 1);
+    localStorage.setItem("registrosEgreso", JSON.stringify(registrosEgreso));
+    alert("Registro eliminado");
+    cargarTabla1();
+  }
+};
+
 const cargarTabla = () => {
   tableBody.innerHTML = "";
-  ingresos.map(function (ingreso, index) {
+  registrosIngreso.map(function (registro, index) {
     let tr = document.createElement("tr");
     tr.classList = "table-success";
-    let celda = `<th scope="row">${ingreso.fecha}</th>
-        <td>${ingreso.nombre}</td>
-        <td class="d-flex justify-content-between align-items-center">${ingreso.monto}
-          <div>
-            <button class="btn btn-primary btn-sm" onclick="editModal(${index})"><i class="fas fa-pencil-alt"></i></button>
-            <button class="btn btn-dark btn-sm"><i class="fas fa-trash-alt"></i></button>
-          </div>
-        </td>`;
+    let celda = `<th scope="row">${registro.fecha}</th>
+            <td>${registro.nombre}</td>
+            <td class="d-flex justify-content-between align-items-center">${registro.monto}
+              <div>
+                <button class="btn btn-primary btn-sm" onclick="editModal(${index})"><i class="fas fa-pencil-alt"></i></button>
+                <button class="btn btn-dark btn-sm" onClick="borrarIngreso(${index})"
+                ><i class="fas fa-trash-alt"></i></button>
+              </div>
+            </td>`;
 
     tr.innerHTML = celda;
     tableBody.appendChild(tr);
   });
 };
+
 const cargarTabla1 = () => {
-  tableBody.innerHTML = "";
-  egresos.map(function (egreso) {
+  tableBody1.innerHTML = "";
+  registrosEgreso.map(function (registro, index) {
     let tr1 = document.createElement("tr");
     tr1.classList = "table-danger";
-    let celda = `<th scope="row">${egreso.fecha1}</th>
-        <td>${egreso.nombre1}</td>
-        <td class="d-flex justify-content-between align-items-center">${egreso.monto1}
+    let celda = `<th scope="row">${registro.fecha}</th>
+        <td>${registro.nombre}</td>
+        <td class="d-flex justify-content-between align-items-center">${registro.monto}
           <div>
-            <button class="btn btn-primary btn-sm" onclick="editModal(${egreso.id1})" ><i class="fas fa-pencil-alt"></i></button>
-            <button class="btn btn-dark btn-sm" ><i class="fas fa-trash-alt"></i></button>
+            <button class="btn btn-primary btn-sm" onclick="editModal1(${index})" ><i class="fas fa-pencil-alt"></i></button>
+            <button class="btn btn-dark btn-sm"  onClick="borrarEgreso(${index})" ><i class="fas fa-trash-alt"></i></button>
           </div>
         </td>`;
 
@@ -141,10 +257,7 @@ const cargarTabla1 = () => {
 
 document
   .getElementById("formulario_ingreso")
-  .addEventListener("submit", agregarIngresos);
-document
-  .getElementById("formulario_egreso")
-  .addEventListener("submit", agregarEgresos);
+  .addEventListener("submit", agregarRegistros);
 
 cargarTabla();
 cargarTabla1();
